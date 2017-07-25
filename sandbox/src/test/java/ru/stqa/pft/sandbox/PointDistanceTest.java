9a59f7cc202fc93a3ed522df214c8e3f4a9f80f3
olgasm3011 <cool-olenka777@rambler.ru>
2017-07-25 23:49:21 +0300

    Задание 3: Два теста для класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointDistanceTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointDistanceTest.java
@@ -2,12 +2,19 @@ package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import static java.lang.StrictMath.sqrt;
 
 public class PointDistanceTest {
   @Test
-  public void testDistance(){
+  public void testDistance1(){
     Point p1 = new Point(2, 1);
     Point p2 = new Point(-1, -3);
     Assert.assertEquals(p2.distance(p1), 5.0);
+  }
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(5, 2);
+    Point p2 = new Point(1, 6);
+    Assert.assertEquals(p2.distance(p1), sqrt(32));
   }
 }
