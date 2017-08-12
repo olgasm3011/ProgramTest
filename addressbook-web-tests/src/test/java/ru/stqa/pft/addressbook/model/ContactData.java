bbbe57e324529530c5e048803303ef2298685973
olgasm3011 <cool-olenka777@rambler.ru>
2017-08-12 21:50:59 +0300

    Лямбда-выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -14,28 +14,10 @@ public class ContactData {
   private final String byear;
   private int id;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
 
-    ContactData that = (ContactData) o;
-
-    if (id != that.id) return false;
-    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = firstname != null ? firstname.hashCode() : 0;
-    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-    result = 31 * result + id;
-    return result;
-  }
 
   public ContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String home, String mobile, String email, String byear) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;;
     this.firstname = firstname;
     this.middlename = middlename;
     this.lastname = lastname;
@@ -74,6 +56,26 @@ public class ContactData {
 
   public int getId() {
     return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (id != that.id) return false;
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    result = 31 * result + id;
+    return result;
   }
 
   public String getFirstname() {
