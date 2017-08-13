418bd22afd10ebb9bcd3cfc80db1d8d281bda593
olgasm3011 <cool-olenka777@rambler.ru>
2017-08-14 00:38:56 +0300

    Задание 11. Тест для проверки информации о контактах на главной странице

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -153,17 +153,19 @@ public class ContactData {
     return this;
   }
 
+  public int getId() {
+    return id;
+  }
+
   @Override
   public String toString() {
     return "ContactData{" +
             "firstname='" + firstname + '\'' +
             ", lastname='" + lastname + '\'' +
-            ", id='" + id + '\'' +
+            ", address='" + address + '\'' +
+            ", email='" + email + '\'' +
+            ", id=" + id +
             '}';
-  }
-
-  public int getId() {
-    return id;
   }
 
   @Override
@@ -175,13 +177,17 @@ public class ContactData {
 
     if (id != that.id) return false;
     if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+    if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+    if (address != null ? !address.equals(that.address) : that.address != null) return false;
+    return email != null ? email.equals(that.email) : that.email == null;
   }
 
   @Override
   public int hashCode() {
     int result = firstname != null ? firstname.hashCode() : 0;
     result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    result = 31 * result + (address != null ? address.hashCode() : 0);
+    result = 31 * result + (email != null ? email.hashCode() : 0);
     result = 31 * result + id;
     return result;
   }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTest.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static ru.stqa.pft.addressbook.tests.TestBase.app;
+
+public class ContactEmailTest extends TestBase {
+  @Test
+  public void testContactEmails() {
+    app.goTo().gotoHomePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    System.out.printf("contact: %s, \n mergeEmails(contactInfoFromEditForm): %s", contact, mergeEmails(contactInfoFromEditForm));
+    assertThat(contact.getEmail(), equalTo(mergeEmails(contactInfoFromEditForm)));
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s) -> ! s.equals(""))
+            .collect(Collectors.joining("\n"));
+  }
+}
